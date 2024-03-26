49fab28d50d0e2df5dd4f1d564032eaf75672217
Ben Wu <wubin28@gmail.com>
2024-03-26 13:24:19 +0800

    feat:ch02: allowing multiple guesses with looping

--- ch02/guess_the_number/src/main.rs
+++ ch02/guess_the_number/src/main.rs
@@ -9,26 +9,27 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
+        let x = 5;
+        let y = 10;
+
+        println!("x = {x} and y + 2 = {}", y + 2);
     }
-
-    let x = 5;
-    let y = 10;
-
-    println!("x = {x} and y + 2 = {}", y + 2);
 }
