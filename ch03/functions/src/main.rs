1751a3fe76755adca8a2655cfb2d4da8426b5dd9
Ben Wu <wubin28@gmail.com>
2024-03-27 17:15:57 +0800

    feat:ch03: function with 2 parameters

--- ch03/functions/src/main.rs
+++ ch03/functions/src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     println!("Hello, world!");
     another_function(5);
+    print_labeled_measurement(5, 'h');
 }
 
 fn another_function(i: i32) {
     println!("The value of i is: {}", i);
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The value of the measurement is: {}{}", value, unit_label);
 }
