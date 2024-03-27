7ddcecb0fa2ee929e8e2424899cd0158b12b3bca
Ben Wu <wubin28@gmail.com>
2024-03-27 21:01:36 +0800

    feat:ch03: conditional loops with while

--- ch03/loops/src/main.rs
+++ ch03/loops/src/main.rs
@@ -31,4 +31,13 @@ fn main() {
         count += 1;
     }
     println!("End count = {count}");
+
+    // conditional loops with while
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
 }
