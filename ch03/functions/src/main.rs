03c29a02b3d92d1aa8dcfe5c34fd850daf9950d1
Ben Wu <wubin28@gmail.com>
2024-03-27 18:18:00 +0800

    feat:ch03: the body of the function is a lonely 5 with no semicolon because it’s an expression whose value we want to return

--- ch03/functions/src/main.rs
+++ ch03/functions/src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     println!("Hello, world!");
     another_function(5);
     print_labeled_measurement(5, 'h');
+
+    let x = five();
+    println!("The value of x is: {}", x);
 }
 
 fn another_function(i: i32) {
@@ -10,4 +13,8 @@ fn another_function(i: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The value of the measurement is: {}{}", value, unit_label);
+}
+
+fn five() -> i32 {
+    5
 }
