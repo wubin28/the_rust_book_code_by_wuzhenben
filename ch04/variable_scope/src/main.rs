18aa35da3cc2464daf4cc6bfae4c0d59baa4cd04
Ben Wu <wubin28@gmail.com>
2024-03-28 10:56:13 +0800

    feat:ch04: push_str() appends a literal to a String

--- ch04/variable_scope/src/main.rs
+++ ch04/variable_scope/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{s}"); // this will print `hello, world!`
 }
