f7bb7ff004d7b04f390a7633e463be63499ee042
Ben Wu <wubin28@gmail.com>
2024-03-31 21:21:46 +0800

    feat:ch04: implement the Drop trait for a custom type

--- ch04/variable_scope/src/main.rs
+++ ch04/variable_scope/src/main.rs
@@ -1,7 +1,22 @@
+struct MyResource {
+    // Some fields
+}
+
+impl Drop for MyResource {
+    fn drop(&mut self) {
+        // Code to release resources or clean up
+        println!("MyResource is being dropped!");
+    }
+}
+
 fn main() {
     let mut s = String::from("hello"); // s is valid from this point forward
 
     s.push_str(", world!"); // push_str() appends a literal to a String
 
     println!("{s}"); // this will print `hello, world!`
+
+    // a variable of a custom type
+    let _my_resource = MyResource {}; // my_resource is valid from this point forward
+                                      // do stuff with my_resource
 } // Here, Rust automatically calls `drop` for `s`, releasing its memory.
