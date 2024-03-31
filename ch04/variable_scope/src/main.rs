99b81be701ce8702b223acbe72b75a4562e7dcc1
Ben Wu <wubin28@gmail.com>
2024-03-31 21:13:25 +0800

    docs:ch04: Rust automatically calls 'drop' for 's', releasing its memory

--- ch04/variable_scope/src/main.rs
+++ ch04/variable_scope/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let mut s = String::from("hello");
+    let mut s = String::from("hello"); // s is valid from this point forward
 
     s.push_str(", world!"); // push_str() appends a literal to a String
 
     println!("{s}"); // this will print `hello, world!`
-}
+} // Here, Rust automatically calls `drop` for `s`, releasing its memory.
