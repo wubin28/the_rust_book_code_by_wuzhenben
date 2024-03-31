980b9fff8e760f388171828b7d7b0a222d75a636
Ben Wu <wubin28@gmail.com>
2024-03-31 23:00:17 +0800

    feat:ch04: This will not work because s1 has been moved to s2

--- ch04/move_a_variable/src/main.rs
+++ ch04/move_a_variable/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    // This will not work because s1 has been moved to s2
+    println!("{s1}, world!");
 }
