7170e0a72cf31831a65c2d7fef6f33befeb81e5e
Ben Wu <wubin28@gmail.com>
2024-04-01 18:31:43 +0800

    feat:ch04: Iterators are one of Rust’s zero-cost abstractions

--- ch04/zero_cost_abstractions/src/main.rs
+++ ch04/zero_cost_abstractions/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Iter Sum: {}", iter_sum());
+
+    println!("Loop Sum: {}", loop_sum());
+}
+
+fn iter_sum() -> i32 {
+    let vec = vec![1, 2, 3, 4];
+    let vec_sum: i32 = vec.iter().sum();
+    vec_sum
+}
+
+fn loop_sum() -> i32 {
+    let vec = vec![1, 2, 3, 4];
+    let mut sum = 0;
+    for x in &vec {
+        sum += x;
+    }
+    sum
 }
