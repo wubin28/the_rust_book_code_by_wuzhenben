c94c4b22d58d456ffac9235e84cd180e9a40a3eb
Ben Wu <wubin28@gmail.com>
2024-04-01 08:02:06 +0800

    feat:ch04: This Rust code attempts to return a reference to a local variable. The compiler will prevent this behavior because it could lead to dangling references.

--- ch04/strict_borrow_checks_during_compilation/src/main.rs
+++ ch04/strict_borrow_checks_during_compilation/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 42;
+    let ref_to_nothing = {
+        let y = 32;
+        &y
+    };
+
+    println!("{}", ref_to_nothing); // 这里会编译错误，因为y在引用离开作用域时不再存在
 }
