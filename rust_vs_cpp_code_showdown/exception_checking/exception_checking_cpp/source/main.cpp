f729dba50f8f4c7218894cdb0a4adcd66ed07814
Ben Wu <wubin28@gmail.com>
2024-04-02 11:12:10 +0800

    feat:ch04:showdown:exception_checking:C++: perform a general catch for exceptions

--- rust_vs_cpp_code_showdown/exception_checking/exception_checking_cpp/source/main.cpp
+++ rust_vs_cpp_code_showdown/exception_checking/exception_checking_cpp/source/main.cpp
@@ -1,12 +1,34 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 
 #include "lib.hpp"
+
+std::string read_file_to_string(const std::string& path)
+{
+  std::ifstream file(path);
+  if (!file.is_open()) {
+    throw std::runtime_error("Could not open file");
+  }
+
+  std::string contents((std::istreambuf_iterator<char>(file)),
+                       std::istreambuf_iterator<char>());
+  return contents;
+}
 
 auto main() -> int
 {
   auto const lib = library {};
   auto const message = "Hello from " + lib.name + "!";
   std::cout << message << '\n';
+
+  // 泛捕获异常
+  try {
+    std::string contents = read_file_to_string("hello.txt");
+    std::cout << "File contents: " << contents << std::endl;
+  } catch (...) {  // 泛捕获异常的错误在这里
+    std::cerr << "An error occurred." << std::endl;
+  }
+
   return 0;
 }
