f9d3d03b33a82d78767d8ebdc3fbe36a878a3c8f
Ben Wu <wubin28@gmail.com>
2024-04-03 23:06:49 +0800

    feat:ch04:C++: got a warning 'array index 10 is past the end of the array (that has type 'int[3]')' when compiling the code using clang++ -std=c++17

--- ch04/cpp/out_of_bounds_array_access_may_lead_to_undefined_behavior/source/main.cpp
+++ ch04/cpp/out_of_bounds_array_access_may_lead_to_undefined_behavior/source/main.cpp
@@ -8,5 +8,9 @@ auto main() -> int
   auto const lib = library {};
   auto const message = "Hello from " + lib.name + "!";
   std::cout << message << '\n';
+
+  int arr[3] = {1, 2, 3};
+  std::cout << arr[10] << std::endl;  // 越界访问，导致未定义行为。
+
   return 0;
 }
