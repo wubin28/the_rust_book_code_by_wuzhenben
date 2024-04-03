d103bc8e01d9f1d38e50a3122267d7ea83c17f6b
Ben Wu <wubin28@gmail.com>
2024-04-04 00:36:12 +0800

    feat:ch04: borrow s as mutable to the change function

--- ch04/borrowing/src/main.rs
+++ ch04/borrowing/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello"); // s is the owner of the string "hello"
+
+    change(&mut s); // we borrow s as mutable to the change function
+
+    println!("{}", s); // s is still the owner and can be used here
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world"); // we can modify some_string because it's a mutable borrow
 }
