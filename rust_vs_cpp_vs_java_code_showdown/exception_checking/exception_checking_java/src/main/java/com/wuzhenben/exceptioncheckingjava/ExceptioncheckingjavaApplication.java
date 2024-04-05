d5201ce7d52e598cecb7d6722bd67f0a1c3b21e8
Ben Wu <wubin28@gmail.com>
2024-04-05 12:51:45 +0800

    chore:ch04:showdown:exception_checking:java: run command 'curl https://start.spring.io/starter.zip -d javaVersion=17 -d bootVersion=3.2.4 -d groupId=com.wuzhenben -d artifactId=exceptioncheckingjava -d name=exceptioncheckingjava -d description="Demo project for exception checking" -d packageName=com.wuzhenben.exceptioncheckingjava -d packaging=jar -d type=gradle-project -o exceptioncheckingjava.zip' and 'unzip exceptioncheckingjava.zip -d exception_checking_java'

---
+++ rust_vs_cpp_vs_java_code_showdown/exception_checking/exception_checking_java/src/main/java/com/wuzhenben/exceptioncheckingjava/ExceptioncheckingjavaApplication.java
@@ -0,0 +1,13 @@
+package com.wuzhenben.exceptioncheckingjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptioncheckingjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptioncheckingjavaApplication.class, args);
+	}
+
+}
---
+++ rust_vs_cpp_vs_java_code_showdown/exception_checking/exception_checking_java/src/test/java/com/wuzhenben/exceptioncheckingjava/ExceptioncheckingjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuzhenben.exceptioncheckingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptioncheckingjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
