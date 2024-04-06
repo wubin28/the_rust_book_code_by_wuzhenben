3979ccda983adb99915877a3b82fe15bda235b74
Ben Wu <wubin28@gmail.com>
2024-04-06 09:29:36 +0800

    chore:ch04:showdown:exception_checking:java8: make the program runnable by 'gradle build' and 'gradle run'

--- rust_vs_cpp_vs_java_code_showdown/exception_checking/exception_checking_java/src/test/java/com/wuzhenben/exceptioncheckingjava/ExceptioncheckingjavaApplicationTests.java
+++ rust_vs_cpp_vs_java_code_showdown/exception_checking/exception_checking_java/src/test/java/com/wuzhenben/exceptioncheckingjava/ExceptioncheckingjavaApplicationTests.java
@@ -3,7 +3,6 @@ package com.wuzhenben.exceptioncheckingjava;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ExceptioncheckingjavaApplicationTests {
 
 	@Test
